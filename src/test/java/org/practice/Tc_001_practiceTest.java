576df5a5aa72f0645fdc2d0a3159ea0dceb5b5e1
Admin <Admin@HP>
2022-09-23 10:35:32 +0530

    commit 1-->uploaded the project completely

---
+++ src/test/java/org/practice/Tc_001_practiceTest.java
@@ -0,0 +1,19 @@
+package org.practice;
+
+import org.testng.annotations.Test;
+
+public class Tc_001_practiceTest {
+
+	@Test
+	public void test1() {
+		System.out.println("pass ");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("pass ");
+	}
+	
+
+	
+}
