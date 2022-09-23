9e9c77925e843f8a260327b85aadf10aca941b16
Admin <Admin@HP>
2022-09-23 11:26:02 +0530

    commit 3

--- src/test/java/org/practice/Tc_001_practiceTest.java
+++ src/test/java/org/practice/Tc_001_practiceTest.java
@@ -6,17 +6,22 @@ public class Tc_001_practiceTest {
 
 	@Test
 	public void test1() {
-		System.out.println("pass ");
+		System.out.println("pass1 ");
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("pass ");
+		System.out.println("pass 2");
 	}
 	
 	@Test
 	public void test3() {
-		System.out.println("pass ");
+		System.out.println("pass 3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("pass4 ");
 	}
 	
 
